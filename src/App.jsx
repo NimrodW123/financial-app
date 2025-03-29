9bf15ef6d6591cf891ffd98c744d417a09142118
Nimrod Wielgus <nimrodwielgus@h-MBP-sl-Nimrod.lan>
2025-03-29 21:12:45 +0300

    העלאה ראשונית

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FinancialManagementPlatform from "./components/FinancialManagementPlatform";
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <FinancialManagementPlatform />
+    </div>
+  );
+}
